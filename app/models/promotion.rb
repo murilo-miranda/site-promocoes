5f1d8f1b07dc10a208f3be5d48f8bc92f1fa7260
m-faulkness <murilomirandamoreno@gmail.com.com>
2021-01-30 15:18:47 -0300

    Admin cadastra uma promoção

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,2 +1,8 @@
 class Promotion < ApplicationRecord
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :code, presence: true
+    validates :discount_rate, presence: true
+    validates :coupon_quantity, presence: true
+    validates :expiration_date, presence: true
 end
