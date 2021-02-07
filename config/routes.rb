59434e3006a462213049454b7a632b145ceb0430
m-faulkness <murilomirandamoreno@gmail.com.com>
2021-02-07 19:34:33 -0300

    User sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
+
+  devise_for :users
+
   resources :promotions do
     post 'generate_coupons', on: :member
   end
