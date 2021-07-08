a41d7c959edfdc8f129005a3872491a873e6b431
m-faulkness <murilomirandamoreno@gmail.com.com>
2021-07-08 16:58:03 -0300

    DONE - Cadastrar uma promoçao

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -6,4 +6,22 @@ class PromotionsController < ApplicationController
   def show
     @promotion = Promotion.find(params[:id])
   end
+
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    @promotion = Promotion.new(promotion_params)
+    @promotion.save!
+
+    redirect_to @promotion
+  end
+
+  private
+
+  def promotion_params
+    params.require(:promotion).permit(:name, :description, :code, :discount_rate,
+                                      :coupon_quantity, :expiration_date)
+  end
 end
